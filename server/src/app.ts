9353efa751acde8e7dc1915927c51c1bf13d8b0c
Kristiyan Merdzhov <88944722+NewPhoneWhoDis@users.noreply.github.com>
2023-10-22 14:20:51 +0200

    Added prod builds

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,20 @@
-import express from 'express';
-import cors from 'cors';
+import path from "path";
+import express from "express";
+import cors from "cors";
 
-import planetsRouter from './routes/planets/planets.router';
-
-
+import planetsRouter from "./routes/planets/planets.router";
 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
